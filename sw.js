90a2ca53a379de421f80244ea10c0fada3dad2b0
Ahmad283980 <mohammed.ahmad3251@gmail.com>
2026-09-21 14:10:59 +0300

    DeutschLern: sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+/* DeutschLern offline service worker */
+const CACHE = 'deutschlern-v1';
+const ASSETS = [
+  './', './index.html', './manifest.json',
+  './icon-192.png', './icon-512.png', './icon-maskable-512.png',
+  './apple-touch-icon-180.png', './favicon-32.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== location.origin) return;
+  e.respondWith(
+    caches.match(req, { ignoreSearch: true }).then(hit => hit || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
